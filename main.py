c5a066c75b894d1f24e5f85343e887428d34e593
Dormouse Young <dormouse.young@gmail.com>
2020-05-07 10:40:36 +0800

    Add a test help dialog.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import urwid
 
-class Todo_Frame(object):
+
+class TodoFrame(object):
     palette = [
         ('body', 'black', 'dark cyan', 'standout'),
         ('foot', 'light gray', 'black'),
         ('key', 'light cyan', 'black', 'underline'),
         ('title', 'yellow', 'black',),
+        ('message', 'yellow', 'black',),
     ]
 
     footer_text = [
@@ -13,32 +15,50 @@ class Todo_Frame(object):
         ('key', "UP"), ", ", ('key', "DOWN"), ", ",
         ('key', "PAGE UP"), " and ", ('key', "PAGE DOWN"),
         " move view  ",
+        ('key', "H"), " helps",
         ('key', "Q"), " exits",
     ]
 
     def __init__(self):
         listbox_content = self.gen_view_contents()
         self.listbox = urwid.ListBox(urwid.SimpleFocusListWalker(listbox_content))
+        self.body = urwid.AttrMap(urwid.Filler(self.listbox, valign="middle",
+                                               height=('relative', 100),
+                                               top=2, min_height=10), 'body')
         footer = urwid.AttrMap(urwid.Text(self.footer_text), 'foot')
-        self.view = urwid.Frame(urwid.AttrWrap(self.listbox, 'body'), footer=footer)
-        self.loop = urwid.MainLoop(self.view, self.palette, unhandled_input=exit_on_q)
+        self.view = urwid.Frame(self.body, footer=footer)
+        self.loop = urwid.MainLoop(self.view,
+                                   self.palette,
+                                   unhandled_input=self.handle_input)
         self.loop.run()
 
-
-    def item_chosen(self, w, index):
-        response = urwid.Text([u'You chose ', str(index), u'\n'])
+    def show_help(self):
         done = urwid.Button(u'Ok')
         urwid.connect_signal(done, 'click', self.dia_but_clicked)
-        # main.original_widget = urwid.Filler(urwid.Pile([response,
-        self.view.body = urwid.Filler(urwid.Pile([response,
-            urwid.AttrMap(done, None, focus_map='reversed')]))
+
+        help_body = [urwid.Text('help'), urwid.Divider(),
+                     urwid.Text('help content'), urwid.Divider(),
+                     done]
+        help_box = urwid.AttrMap(
+            urwid.ListBox(urwid.SimpleFocusListWalker(help_body)),
+            'message'
+        )
+        self.body.original_widget = urwid.Overlay(
+            help_box,
+            self.body.original_widget,
+            align='center', width=('relative', 50),
+            valign='middle', height=('relative', 50),
+        )
+
+    def item_chosen(self, w, index):
+        pass
 
     def gen_view_contents(self):
         todo_items = [
             '+1st buy milk',
             '-2rd clean body',
             '-3rd go to school',
-            '-测试一下很长很长的内容，这个内容真的很长。'*3
+            '-测试一下很长很长的内容，这个内容真的很长。' * 3
         ]
         contents = [
             urwid.Columns(
@@ -56,7 +76,7 @@ class Todo_Frame(object):
             else:
                 is_done_txt = '[ ]'
 
-            but_txt = '   '.join([str(index) ,is_done_txt, item[1:]])
+            but_txt = '   '.join([str(index), is_done_txt, item[1:]])
             but = urwid.Button(but_txt)
             urwid.connect_signal(but, 'click', self.item_chosen, index)
             contents.append(urwid.AttrMap(but, None, focus_map='reversed'))
@@ -64,20 +84,14 @@ class Todo_Frame(object):
         return contents
 
     def dia_but_clicked(self, button):
-        self.view.body = self.listbox
+        self.body.original_widget = self.listbox
 
-
-def exit_program(button):
-    raise urwid.ExitMainLoop()
-
-
-def exit_on_q(input_char):
-    if input_char in ('q', 'Q'):
-        raise urwid.ExitMainLoop()
-
-
+    def handle_input(self, input_char):
+        if input_char in ('q', 'Q'):
+            raise urwid.ExitMainLoop()
+        if input_char in ('h', 'H'):
+            self.show_help()
 
 
 if __name__ == "__main__":
-    Todo_Frame()
-
+    TodoFrame()
